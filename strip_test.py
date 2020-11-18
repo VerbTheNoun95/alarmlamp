f7d60a9992ad1e9d1d9ef8e9da7020869418e542
VerbTheNoun95 <abartnik@hotmail.com>
2020-11-17 20:31:00 -0500

    Forcing python 3

--- strip_test.py
+++ strip_test.py
@@ -1,6 +1,9 @@
+#!/usr/bin/python3
+
 from bibliopixel.layout import *
 from bibliopixel.animation import StripChannelTest
 from bibliopixel.drivers.SPI.LPD8806 import *
+
 
 #create driver for a 32 pixels
 driver = LPD8806(32, c_order=ChannelOrder.BRG)
