5fcf6cecaaea4367cb5a1a6a4e76c34deb8b96ac
VerbTheNoun95 <abartnik@hotmail.com>
2020-11-17 19:05:25 -0500

    Fixed channel order for SPI driver

--- strip_test.py
+++ strip_test.py
@@ -3,7 +3,7 @@ from bibliopixel.animation import StripChannelTest
 from bibliopixel.drivers.SPI.LPD8806 import *
 
 #create driver for a 32 pixels
-driver = LPD8806(32)
+driver = LPD8806(32, c_order=ChannelOrder.BRG)
 led = Strip(driver)
 
 anim = StripChannelTest(led)
