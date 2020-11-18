3bb31db0a69672c24e053cb2d5d1edc4faf5889c
VerbTheNoun95 <abartnik@hotmail.com>
2020-11-17 19:03:40 -0500

    Added strip test from bibliopixel docs

---
+++ strip_test.py
@@ -0,0 +1,11 @@
+from bibliopixel.layout import *
+from bibliopixel.animation import StripChannelTest
+from bibliopixel.drivers.SPI.LPD8806 import *
+
+#create driver for a 32 pixels
+driver = LPD8806(32)
+led = Strip(driver)
+
+anim = StripChannelTest(led)
+anim.run()
+
