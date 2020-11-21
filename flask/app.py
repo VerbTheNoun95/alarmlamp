76f2ebe8c60dba92bf7622f3529d616972a13b9b
VerbTheNoun95 <abartnik@hotmail.com>
2020-11-21 17:39:55 -0500

    Added scheduling to run script when user enters input in flask app

--- flask/app.py
+++ flask/app.py
@@ -1,17 +1,53 @@
 from flask import Flask, render_template, request, url_for, redirect
+from apscheduler.schedulers.background import BackgroundScheduler
 import json
+import os
+import time
+from time import sleep
+from datetime import datetime
+
 
 app = Flask(__name__)
 
 hours = [ '0' + str(i) if len(str(i)) == 1 else str(i) for i in list(range(0,24)) ]
 minutes = [ '0' + str(i) if len(str(i)) == 1 else str(i) for i in list(range(0,60)) ]
 
+sched = BackgroundScheduler(daemon=True)
+sched.start()
+
+def get_date(verbose=False):
+    year = datetime.date(datetime.now()).year
+    month = datetime.date(datetime.now()).month
+    day = datetime.date(datetime.now()).day
+
+    if verbose == True:
+        print(year, month, day)
+
+    return("{}-{}-{}".format(year, month, day))
+
+def hello():
+    for i in range(0,5):
+        print(i)
+        sleep(1)
+
+if os.path.exists("time.json"):
+    with open('time.json', 'r') as time_file:
+        line = time_file.readline()
+        time = json.loads(line)
+
+    hour = time['hour']
+    minute = time['minute']
+
+    start_date = "{} {}:{}:00".format(get_date(), hour, minute)
+    print(start_date)
+    sched.add_job(hello, trigger='interval', seconds=10, id='alarm', start_date=start_date)
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
     return render_template("dropdown.html", hours=hours, minutes=minutes)
 
 @app.route('/time', methods=['GET', 'POST'])
-def login():
+def time():
 
     if request.method == "POST":
         hour = request.form.get("hours", None)
@@ -21,9 +57,15 @@ def login():
             json.dump(output, outfile)
 
         if hour != None and minute != None:
+            sched.remove_job('alarm')
+
+            start_date = "{} {}:{}:00".format(get_date(), hour, minute)
+            print(start_date)
+            sched.add_job(hello, trigger='interval', seconds=10, id='alarm', start_date=start_date)
+
             return render_template("dropdown.html", hours=hours, minutes=minutes, hour=hour, minute=minute)
 
     return render_template("dropdown.html", hours=hours, minutes=minutes, hour=hour, minute=minute)
 
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0")
+    app.run(debug=False, host="0.0.0.0")
